4be7edec664e89d993559a14a5ecebc04d9246b5
Pavel <pavel.mokr2005@gmail.com>
2023-01-21 22:13:57 +0500

    view update

--- AboutCSharpDeveloper/views.py
+++ AboutCSharpDeveloper/views.py
@@ -2,19 +2,23 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import *
 from .HeadHunterLoader import get_c_sharp_vacs
+from .forms import DateForm
+from datetime import date, timedelta
 
 
 def index(request):
     context = {
+        'title': "Главная страница",
         'chartPages': PageForCharts.objects.all()
     }
     return render(request, 'AboutCSharpDeveloper/index.html', context)
 
 
 def show_page_for_chart(request, page_for_charts_slug):
-    page_for_charts_id = PageForCharts.objects.get(slug=page_for_charts_slug).id
-    charts = Chart.objects.filter(page=page_for_charts_id)
+    page_for_charts = PageForCharts.objects.get(slug=page_for_charts_slug)
+    charts = Chart.objects.filter(page=page_for_charts.id)
     context = {
+        'title': page_for_charts.name,
         'charts': charts,
         'chartPages': PageForCharts.objects.all(),
     }
@@ -22,17 +26,41 @@ def show_page_for_chart(request, page_for_charts_slug):
 
 
 def show_chart(request, chart_slug):
+    chart = Chart.objects.get(slug=chart_slug)
     context = {
-        'chart': Chart.objects.get(slug=chart_slug),
+        'title': chart.name,
+        'chart': chart,
         'chartPages': PageForCharts.objects.all()
     }
     return render(request, 'AboutCSharpDeveloper/chart.html', context)
 
 
+vacs = {}
+search_date = date(2023, 1, 1)
+
+
 def recent_vacancies(request):
-    vacs = get_c_sharp_vacs(2022, 12, 20).to_dict('records')
+    global vacs
+    global search_date
     context = {
-        'chartPages': PageForCharts.objects.all(),
-        'vacs': vacs
+        'title': "Последние вакансии",
+        'chartPages': PageForCharts.objects.all()
     }
+    if request.method == 'POST':
+        form = DateForm(request.POST)
+        if form.is_valid():
+            new_search_date = form.cleaned_data['date']
+            if new_search_date == search_date:
+                form.add_error('date', 'Поиск по этой дате уже осуществлён')
+            else:
+                search_date = new_search_date
+                if search_date >= date.today() or search_date <= date.today() - timedelta(30):
+                    form.add_error('date', 'Указана неверная дата')
+                else:
+                    vacs = get_c_sharp_vacs(search_date).to_dict('records')
+
+    else:
+        form = DateForm()
+    context['form'] = form
+    context['vacs'] = vacs
     return render(request, 'AboutCSharpDeveloper/recent_vacancies.html', context)
